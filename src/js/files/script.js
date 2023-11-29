c2305f4a8ecfa18440774cc43f12ab96c9f1015e
unknown <krutko7779@gmail.com>
2023-11-29 19:32:23 +0300

    corrected img

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -5,7 +5,7 @@ import { flsModules } from "./modules.js";
 
 // Переход на страницу благодарности после отправки формы
 document.addEventListener( 'formSent', function( event ) {
-	location = 'https://webkrutko.by/thank-you-page.html';
+	location = 'https://portfolio.webkrutko.by/thank-you-page.html';
 }, false );
 
 // Защита формы от ботов через пустое поле
@@ -16,22 +16,6 @@ if ( document.querySelector('.btn') ) {  // Проверяем наличие э
 			code.value = 'NOSPAM'; // Подставляем значение в value инпута	
 	};
 }
-  // Форма в popup
-  if ( document.querySelector('.btn-popup') ) {  // Проверяем наличие элемента на странице
-		let codePopup = document.querySelector('#code-popup'); // Получаем скрытый input
-		document.querySelector('.btn-popup').onclick = function(){ // Клик по кнопке отправки
-			codePopup.value = 'NOSPAM'; // Подставляем значение в value инпута	
-		};
-  }
-
-    // Форма в брифе
-	 if ( document.querySelector('.btn-brief') ) {  // Проверяем наличие элемента на странице
-		let codePopup = document.querySelector('#code-brief'); // Получаем скрытый input
-		document.querySelector('.btn-brief').onclick = function(){ // Клик по кнопке отправки
-			codePopup.value = 'NOSPAM'; // Подставляем значение в value инпута	
-		};
-  }
-
   // Кнопка НАЗАД
 if ( document.querySelector('.button-back') ) {  // Проверяем наличие элемента на странице
 	document.querySelector('.button-back').onclick = function(){ // Клик по кнопке НАЗАД
